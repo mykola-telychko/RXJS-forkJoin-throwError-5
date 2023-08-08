dfb8b79d76bd58669a5024f3519d55cecbed92ee
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-08 05:20:23 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,25 +1,20 @@
 import { delay, catchError } from 'rxjs/operators';
 import { forkJoin, of, throwError } from 'rxjs';
 
+// https://www.learnrxjs.io/learn-rxjs/operators/combination/forkjoin
 /*
   Emit values from successfully completed
   inner observables.
 */
 const example = forkJoin({
   // emit 'Hello' immediately
-  sourceOne: of('Hello'),
+  srcOne: of('Hello'),
   // emit 'World' after 1 second
-  sourceTwo: of('World').pipe(delay(1000)),
+  srcTwo: of('World').pipe(delay(6000)),
   // throw error
-  sourceThree: throwError('This will error').pipe(catchError(error => of(error)))
+  srcThree: throwError('This will error').pipe(
+    catchError((error) => of(error))
+  ),
 });
 
-/*
- * Output:
- * {
- *   sourceOne: "Hello",
- *   sourceTwo: "World",
- *   sourceThree: "This will error"
- * }
- */
-const subscribe = example.subscribe(val => console.log(val));
+const subscribe = example.subscribe((val) => console.log(val));
